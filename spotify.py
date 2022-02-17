bc5e8445b97205fa7aa0388509bcd6f88ff4f20c
Amirhossein Banavi <ahbanavi@gmail.com>
2022-02-17 16:21:55 +0330

    finish!

--- spotify.py
+++ spotify.py
@@ -1,7 +1,8 @@
+from dotenv import load_dotenv
+import json
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -19,7 +20,32 @@ sp = spotipy.Spotify(
     )
 )
 
-PLAY_LIST_ID = "6vBHSjzJHvMkiayb7IoOSw"
+PLAY_LIST_ID = os.getenv("PLAY_LIST_ID")
+
+
+# get playlist items
+def get_playlist_tracks(playlist_id=PLAY_LIST_ID, limit=100, offset=0, track_ids=[]):
+    results = sp.playlist_items(
+        playlist_id=playlist_id,
+        additional_types=("track",),
+        fields="items(track(id)), next",
+        limit=limit,
+        offset=offset,
+    )
+
+    for track in results["items"]:
+        track_ids.append(track["track"]["id"])
+
+    if results["next"] is None:
+        return track_ids
+
+    return get_playlist_tracks(
+        playlist_id=playlist_id,
+        limit=limit,
+        offset=offset + limit,
+        track_ids=track_ids,
+    )
+
 
 # spotify add song to playlist
 def add_song_to_playlist(tracks, playlist_id=PLAY_LIST_ID):
@@ -27,17 +53,88 @@ def add_song_to_playlist(tracks, playlist_id=PLAY_LIST_ID):
     if not isinstance(tracks, list):
         tracks = [tracks]
 
-    sp.user_playlist_add_tracks(user="", playlist_id=playlist_id, tracks=tracks)
+    old_tracks = get_playlist_tracks(playlist_id=playlist_id)
+    track_to_add = [track for track in tracks if track not in old_tracks]
+    if not track_to_add:
+        print("All songs are already in playlist!")
+        return
+
+    sp.playlist_add_items(playlist_id=playlist_id, items=track_to_add)
+    print("All songs added to playlist.")
 
 
+char_filter_list = ("(", ")", "-", "_", '"', "'", "’", "?")
 # get song id from search query
-def get_song_id(song_name, artist=""):
-    query = "artist:" + artist + " track:" + song_name
+def get_song_id(song_name, artist="", original_artist=""):
+    song_name = "".join(ch for ch in song_name if ch not in char_filter_list)
+    query = song_name
+    if artist:
+        query += " " + artist
     results = sp.search(q=query, type="track")
     items = results["tracks"]["items"]
+
+    if not items:
+        # try one more time without artist name
+        if artist != "":
+            return get_song_id(song_name, "", artist)
+        print("Cannot find song:", query, "|" + original_artist)
+        return None
+
     for item in items:
-        if item["name"] == song_name:
+        item_name = "".join(ch for ch in item["name"] if ch not in char_filter_list)
+        if item_name.lower() == song_name.lower():
             return item["id"]
 
+    input_ = ""
+    item_id = 0
+    while (input_ != "y") and (input_ != "n"):
+        if item_id >= len(items):
+            input_ = input("Song not found. explicitly set uri? (empty for cancel) ")
+            if input_ == "":
+                return None
+            return input_
 
-add_song_to_playlist(get_song_id("This Year's Love", "David Gray"))
+        item = items[item_id]
+        input_ = input(
+            f"Song title `{item['name']}` by `{item['artists'][0]['name']}` for `{query} {original_artist}`? (y/n/empty for next) "
+        )
+        if input_.lower() == "y":
+            return item["id"]
+        else:
+            item_id += 1
+
+    return None
+
+
+def find_song_ids(data):
+    for item in data:
+        if item.get("uri", False):
+            continue
+
+        song_id = get_song_id(item["song"], item["artist"])
+        if song_id:
+            item["uri"] = song_id
+
+    return data
+
+
+# load data
+with open("data.json", "r") as json_file:
+    data = json.load(json_file)
+
+
+def write_uri():
+    data["items"] = find_song_ids(data["items"])
+
+    # write data
+    with open("data.json", "w") as outfile:
+        json.dump(data, outfile)
+
+
+def add_songs_from_uri():
+    # add songs to playlist
+    uris = []
+    for item in data["items"]:
+        uris.append(item["uri"])
+
+    add_song_to_playlist(uris)
--- telegram.py
+++ telegram.py
@@ -1,17 +1,75 @@
-import os
-from dotenv import load_dotenv
 import asyncio
+from dotenv import load_dotenv
+import json
+import os
 from telethon import TelegramClient
 
+load_dotenv()
+
 # Use your own values from my.telegram.org
-api_id = 12345
-api_hash = "0123456789abcdef0123456789abcdef"
+APP_ID = os.getenv("TELEGRAM_APP_ID")
+HASH_ID = os.getenv("TELEGRAM_HASH_ID")
+
+TARGET_CHAT = os.getenv("TELEGRAM_TARGET_CHAT")
+
+# read data from json
+try:
+    with open("data.json", "r") as json_file:
+        data = json.load(json_file)
+except:
+    data = {"items": [], "min_id": 0}
 
 
 async def main():
     # The first parameter is the .session file name (absolute paths allowed)
-    async with TelegramClient("anon", api_id, api_hash).start() as client:
-        await client.send_message("me", "Hello, myself!")
+    client = TelegramClient(".tele", APP_ID, HASH_ID)
+    await client.start()
+    messages = client.iter_messages(TARGET_CHAT, min_id=data["min_id"])
+    is_first = True
+    new_songs = 0
+    async for message in messages:
+        if is_first:
+            data["min_id"] = message.id
+            is_first = False
+
+        if not hasattr(message, "media"):
+            continue
+
+        document = getattr(message.media, "document", None)
+        if not document:
+            continue
+
+        if document.mime_type in ("audio/mpeg", "audio/m4a", "audio/mp4"):
+
+            # for handling files
+            if not hasattr(document.attributes[0], "title"):
+                title = document.attributes[0].file_name.strip(".mp3,.m4a")
+            else:
+                title = document.attributes[0].title
+
+            splits = (
+                title.split("-")
+                if "-" in title
+                else (document.attributes[0].performer, title)
+            )
+            artist, song = splits[0].strip(), splits[1].strip()
+
+            new_item = {"song": song, "artist": artist}
+            if new_item not in data["items"]:
+                print("> new song found:", new_item)
+                data["items"].append(new_item)
+                new_songs += 1
+            else:
+                print(">>> duplicate: ", new_item)
+
+    if new_songs:
+        with open("data.json", "w") as outfile:
+            json.dump(data, outfile)
+            print(f"{new_songs} New songs added to data.json")
+    else:
+        print("No new songs")
+
+    await client.disconnect()
 
 
 asyncio.run(main())
